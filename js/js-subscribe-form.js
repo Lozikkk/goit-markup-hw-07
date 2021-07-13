14628c247e1c7a3c9ea9ed20c27d128b96484976
Oleksandr Lozinskij <68775142+Lozikkk@users.noreply.github.com>
2021-07-13 10:13:04 +0300

    сщььше

---
+++ js/js-subscribe-form.js
@@ -0,0 +1,7 @@
+(() => {
+  document.querySelector('.js-subscribe-form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
+  });
+})();
